3f290c4578556ede86a2420a269c16fbb0d0a6d8
frank qin <miriamandeasonlyric@gmail.com>
2023-11-04 21:09:49 -0400

    create navbar logo and create part(button&img)

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import "./index.css"
 // import { cards, message, request, profile, highline } from "@/js/database"
 
-/* NAV */
+import { profile } from './data/db.json'
 
+import {
+    // NavBar
+    navbar_container_logo,
+    navbar_container_create_img
+} from "@/js/common"
+
+/* NAV */
+navbar_container_logo.textContent = "Topsocial";
+navbar_container_create_img.src = profile.img_src;
 /* LEFT */
 
 /* MIDDLE */
--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,10 @@
 /* NavBar */
+export const navbar = document.querySelector('nav');
+export const navbar_container = navbar.querySelector('.container');
+export const navbar_container_logo = navbar_container.querySelector('.logo');
+export const navbar_container_searchBar = navbar_container.querySelector('.search-bar');
+export const navbar_container_create = navbar_container.querySelector('.create');
+export const navbar_container_create_img = navbar_container.querySelector('img');
 
 
 /* Main */
